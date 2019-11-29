350e33aec59e4f99389d71b48a7ad476dcd69b91
Elvis Souza <souzaelvis33@gmail.com>
2019-11-29 00:09:27 -0300

    upando essa bixiga

---
+++ scripts/processElementData.js
@@ -0,0 +1,48 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["__uRBYRHrEeqeeeZ647sXLQ,_F3tE0RHsEeqeeeZ647sXLQActivity"]="./../../Design Thinking/deliveryprocesses/resources/prototipacao_BFD7B787_84a0d200_Activity.jpeg"
+contentPage.processPage.imageFiles["__uRBYRHrEeqeeeZ647sXLQ,_CoTJkRHsEeqeeeZ647sXLQActivity"]="./../../Design Thinking/deliveryprocesses/resources/imersao_7DD84BF0_42a16669_Activity.jpeg"
+contentPage.processPage.imageFiles["__uRBYRHrEeqeeeZ647sXLQ,_EjENIRHsEeqeeeZ647sXLQActivity"]="./../../Design Thinking/deliveryprocesses/resources/imersao profuda_D4B4E4FE_997dff77_Activity.jpeg"
+contentPage.processPage.imageFiles["__uRBYRHrEeqeeeZ647sXLQ,_HblSYRHsEeqeeeZ647sXLQActivity"]="./../../Design Thinking/deliveryprocesses/resources/ideacao_9A981C01_5f61367a_Activity.jpeg"
+contentPage.processPage.imageFiles["__uRBYRHrEeqeeeZ647sXLQActivity"]="./../../Design Thinking/deliveryprocesses/resources/ciclo-de-vida_175A6B25_175a6b25_Activity.jpeg"
+contentPage.processPage.imageFiles["__uRBYRHrEeqeeeZ647sXLQ,_BY5-QRHsEeqeeeZ647sXLQActivity"]="./../../Design Thinking/deliveryprocesses/resources/analise_927678B7_573f9330_Activity.jpeg"
+contentPage.processPage.elementUrls["_6b-ZAhJGEeq6ptbwIHG6qQ"] = ["Protótipo","Design Thinking/deliveryprocesses/prototipo_41BC1908.html"];
+contentPage.processPage.elementUrls["_RxG7whHsEeqeeeZ647sXLQ"] = ["Mural de Possibilidades","Design Thinking/deliveryprocesses/mural_de_possibilidades_EFA1662E.html"];
+contentPage.processPage.elementUrls["__uRBYRHrEeqeeeZ647sXLQ"] = ["ciclo-de-vida","Design Thinking/deliveryprocesses/ciclo-de-vida_175A6B25.html"];
+contentPage.processPage.elementUrls["_HblSYRHsEeqeeeZ647sXLQ"] = ["ideacao","Design Thinking/deliveryprocesses/ideacao_9A981C01.html"];
+contentPage.processPage.elementUrls["_RxG7wBHsEeqeeeZ647sXLQ"] = ["Brainstorming","Design Thinking/deliveryprocesses/brainstorming_F66A7208.html"];
+contentPage.processPage.elementUrls["_OYcW0hHsEeqeeeZ647sXLQ"] = ["Reenquadramento","Design Thinking/deliveryprocesses/reenquadramento_5C146F9D.html"];
+contentPage.processPage.elementUrls["_1cNTYRJGEeq6ptbwIHG6qQ"] = ["validacao","Design Thinking/deliveryprocesses/validacao_36AADFF9.html"];
+contentPage.processPage.elementUrls["_TpNF0RHsEeqeeeZ647sXLQ"] = ["Mapa Conceitual","Design Thinking/deliveryprocesses/mapa_conceitual_873EFB9D.html"];
+contentPage.processPage.elementUrls["_RxG7xBHsEeqeeeZ647sXLQ"] = ["Workshop de Cocriação","Design Thinking/deliveryprocesses/workshop_de_cocriacao_BAFC5427.html"];
+contentPage.processPage.elementUrls["_CoTJkRHsEeqeeeZ647sXLQ"] = ["imersao","Design Thinking/deliveryprocesses/imersao_7DD84BF0.html"];
+contentPage.processPage.elementUrls["_TpNF0BHsEeqeeeZ647sXLQ"] = ["Definição de Persona","Design Thinking/deliveryprocesses/definicao_de_persona_7CA11B8D.html"];
+contentPage.processPage.elementUrls["_VBa18BHsEeqeeeZ647sXLQ"] = ["Mockups e maquetes","Design Thinking/deliveryprocesses/mockups_e_maquetes_BCC868CD.html"];
+contentPage.processPage.elementUrls["_6b-ZABJGEeq6ptbwIHG6qQ"] = ["Canvas","Design Thinking/deliveryprocesses/canvas_488524E2.html"];
+contentPage.processPage.elementUrls["_TpNF0hHsEeqeeeZ647sXLQ"] = ["Mapa de Empatia","Design Thinking/deliveryprocesses/mapa_de_empatia_75D80FB3.html"];
+contentPage.processPage.elementUrls["_RxG7wRHsEeqeeeZ647sXLQ"] = ["Matriz de Alinhamento","Design Thinking/deliveryprocesses/matriz_de_alinhamento_1085218.html"];
+contentPage.processPage.elementUrls["_OYcW0BHsEeqeeeZ647sXLQ"] = ["Pesquisa Desk","Design Thinking/deliveryprocesses/pesquisa_desk_62DD7B77.html"];
+contentPage.processPage.elementUrls["_QLb-cRHsEeqeeeZ647sXLQ"] = ["Cadernos de Sensibilização","Design Thinking/deliveryprocesses/cadernos_de_sensibilizacao_5A3C855C.html"];
+contentPage.processPage.elementUrls["_VBa18RHsEeqeeeZ647sXLQ"] = ["Protótipo em papel","Design Thinking/deliveryprocesses/prototipo_em_papel_C76648DD.html"];
+contentPage.processPage.elementUrls["_TpNF0xHsEeqeeeZ647sXLQ"] = ["Mapa Mental","Design Thinking/deliveryprocesses/mapa_mental_8075EFC3.html"];
+contentPage.processPage.elementUrls["_BY5-QRHsEeqeeeZ647sXLQ"] = ["analise","Design Thinking/deliveryprocesses/analise_927678B7.html"];
+contentPage.processPage.elementUrls["_OYcW0RHsEeqeeeZ647sXLQ"] = ["Pesquisa Exploratória","Design Thinking/deliveryprocesses/pesquisa_exploratria_6D7B5B87.html"];
+contentPage.processPage.elementUrls["_F3tE0RHsEeqeeeZ647sXLQ"] = ["prototipacao","Design Thinking/deliveryprocesses/prototipacao_BFD7B787.html"];
+contentPage.processPage.elementUrls["_RxG7wxHsEeqeeeZ647sXLQ"] = ["Seleção de Ideias","Design Thinking/deliveryprocesses/selecao_de_ideias_FA3F463E.html"];
+contentPage.processPage.elementUrls["_QLb-cBHsEeqeeeZ647sXLQ"] = ["9 por ques","Design Thinking/deliveryprocesses/9_por_ques_4F9EA54C.html"];
+contentPage.processPage.elementUrls["_EjENIRHsEeqeeeZ647sXLQ"] = ["imersao profuda","Design Thinking/deliveryprocesses/imersao profuda_D4B4E4FE.html"];
+contentPage.processPage.elementUrls["_QLb-cxHsEeqeeeZ647sXLQ"] = ["Sombra","Design Thinking/deliveryprocesses/sombra_53737982.html"];
+contentPage.processPage.elementUrls["_VBa18hHsEeqeeeZ647sXLQ"] = ["Storyboard","Design Thinking/deliveryprocesses/storyboard_B5FF5CF3.html"];
+contentPage.processPage.elementUrls["_QLb-chHsEeqeeeZ647sXLQ"] = ["Entrevistas","Design Thinking/deliveryprocesses/entrevistas_48D59972.html"];
+contentPage.processPage.elementUrls["_6b-ZARJGEeq6ptbwIHG6qQ"] = ["MVP","Design Thinking/deliveryprocesses/mvp_532304F2.html"];
